9df7531c708d265d31c22ab01eec75fd84ef7ceb
tmolinab <105464621+tmolinab@users.noreply.github.com>
2022-05-17 16:19:09 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,111 @@
+# Local URL: http://localhost:8501 Network URL: http://192.168.1.2:8501
+# pagina: https://share.streamlit.io/andersonstiwardgonzalez/b-phages/main/app.py
+import streamlit as st
+
+st.title("B-phages")
+st.image("_MG_1613.jpg")
+st.header("Tu salud, nuestro compromiso")
+st.write("Queremos darte el mejor de los tratamientos contra las infecciones por bacterias multiresistentes")
+st.write("Somos los mejores")
+st.write("[Aprende más de como ser cool --->](https://www.youtube.com/watch?v=VqgUkExPvLY)")
+
+with st.container():
+    st.write("---")
+    st.title("Equipo de trabajo")
+    ana_column, carolina_column, tomas_column, anderson_column = st.columns(4)
+    with ana_column:
+
+        st.write("Ana Maria Ruiz León")
+        st.image("_MG_1613.jpg")
+        st.write("Gerente de la empresa")
+        st.write("[Conoce más de Ana](https://co.linkedin.com/)")
+        
+        with carolina_column:
+            st.write("Diana Carolina Giralda Valderrama")
+            st.image("_MG_1613.jpg")
+            st.write("Jefe de Mercadeo de la empresa")
+            st.write("[Conoce más de Carolina](https://co.linkedin.com/)")
+            
+            with tomas_column:
+                st.write("Tomas Molina Benjumea")
+                st.image("_MG_1613.jpg")
+                st.write("Jefe de Calidad de la empresa")
+                st.write("[Conoce más de Tomas](https://co.linkedin.com/)")
+                with anderson_column:
+                    st.write("Anderson Stiward González Rivera")
+                    st.image("_MG_1613.jpg")
+                    st.write("Jefe de talento humano de la empresa")
+                    st.write("[Conoce más de Anderson](https://co.linkedin.com/)")
+
+
+with st.container():
+    st.write("---")
+    st.write("[B-phages](https://www.youtube.com/)")
+
+with st.container():
+    st.write("---")
+    st.title("Misión")
+    st.write("Somos una empresa de base biotecnológica comprometida con la salud pública. Buscamos generar tratamientos efectivos para infecciones bacterianas mediante el uso de la medicina personalizada con bacteriófagos. Estamos interesados en mitigar la problemática causada por la resistencia a los antibióticos, apoyándonos en nuevas tecnologías que nos permitan tener un buen acompañamiento y la mejor atención a nuestros usuarios. Trabajamos de la mano de la bioética con valores que reafirman nuestro compromiso como son la calidad, el respeto y la honestidad.")
+    st.title("Visión")
+    st.write("Para el año 2032 nos proyectamos como una empresa independiente, posicionada en el mercado colombiano, con miras hacia un reconocimiento internacional en la industria biotecnológica enfocada a la medicina personalizada, siendo pioneros en latinoamérica en el tratamiento de infecciones bacterianas y respetando los fundamentos éticos que nos permitieron un reconocimiento en el territorio nacional.")
+
+with st.container():
+    st.write("---")
+    st.title("Principios y valores")
+    st.write("misionintesrtezasfsfs")
+
+with st.container():
+    st.write("---")
+    st.title("Código CIIU")
+    st.write("misionintesrtezasfsfs")
+
+with st.container():
+    st.write("---")
+    st.title("Lienzo de canvas")
+    st.write("misionintesrtezasfsfs")
+
+with st.container():
+    st.write("---")
+    st.title("Análisis del sector")
+    st.write("misionintesrtezasfsfs")
+
+with st.container():
+    st.write("---")
+    st.title("Matriz D.O.F.A")
+    st.write("misionintesrtezasfsfs")
+
+with st.container():
+    st.write("---")
+    st.title("Productos")
+    st.write("misionintesrtezasfsfs")
+
+with st.container():
+    st.write("---")
+    st.title("Competencia: Modelo de Porter")
+    st.write("misionintesrtezasfsfs")
+
+with st.container():
+    st.write("---")
+    st.title("Plan de mercadeo 4ps")
+    st.write("misionintesrtezasfsfs")
+
+with st.container():
+    st.write("---")
+    st.title("Plan financiero")
+    st.write("misionintesrtezasfsfs")
+
+with st.container():
+    st.write("---")
+    st.title("Impactos sociales y externalidades")
+    st.write("misionintesrtezasfsfs")
+
+with st.container():
+    st.write("---")
+    st.title("¡Contáctanos!")
+    i_column, w_column, c_column, y_column = st.columns(4)
+    with i_column:
+        st.write("Instagram")
+        st.image("_MG_1613.jpg")
+        with w_column:
+            st.write("WhatsApp")
+            st.image("_MG_1613.jpg")
